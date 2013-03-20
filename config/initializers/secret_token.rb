ec32304805936daab22049ac3c9171b97b434401
Leo <vl.tawilian@gmail.com>
2013-03-20 14:58:47 -0400

    initial commit for practice app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practice::Application.config.secret_token = 'a221b88110dc8f767862a84de4f4cee5d386245d3b9a8dd5b6a56ec5686f551811328a9a1168a8bae13dc7504dd77b5bf2fd702193d565c2d81be63b5631ae40'
